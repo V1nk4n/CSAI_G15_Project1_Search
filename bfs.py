3ded5d060851858d3b121de905b779ef4d9c85b7
V1nk4n <vinhan188@gmail.com>
2024-11-02 09:35:54 +0700

    test_input-01.txt

--- bfs.py
+++ bfs.py
@@ -2,11 +2,9 @@ from queue import Queue
 from maze import WALL, FREE, STONE, ARES, SWITCH, STONE_ON_SWITCH, ARES_ON_SWITCH, Stone
 from node import Node
 
-NODES = 0
-
 
 def bfs(maze, ares_start, stones, switches):
-    global NODES
+    nodes = 0
 
     initial_state = Node(maze, ares_start, stones, switches)
 
@@ -16,7 +14,7 @@ def bfs(maze, ares_start, stones, switches):
 
     frontier = Queue()
     frontier.put(initial_state)
-    NODES += 1
+    nodes += 1
     # Lưu các trạng thái để dễ dàng kiểm tra một trạng thái đã có trong frontier hay chưa
     frontier_set = set()
     frontier_set.add(initial_state)
@@ -26,7 +24,7 @@ def bfs(maze, ares_start, stones, switches):
         current_state = frontier.get()
         frontier_set.remove(current_state)
         expanded.add(current_state)
-        NODES += 1
+        nodes += 1
         # Lấy các trạng thái có thể có từ trạng thái hiện tại
         neighbors = current_state.get_neighbors()
 
@@ -41,9 +39,9 @@ def bfs(maze, ares_start, stones, switches):
                         path.append(goal_state)
                         goal_state = goal_state.prev_state
                     # Trả về đường đi từ trạng thái đầu -> đích
-                    return path[::-1], NODES
+                    return path[::-1], nodes
 
                 frontier.put(neighbor)
                 frontier_set.add(neighbor)
 
-    return None
+    return None, nodes
